22053bdc88488a4a16c13ba68e4b91652bc0bfef
xiaoyunlong <1758217010@qq.com>
2020-07-31 20:14:40 +0800

    [flyway][LeBron/Benjamin] feature: add employee and company data

---
+++ src/main/resources/db/migration/V20200731201359__add_company_and_employee_data.sql
@@ -0,0 +1,6 @@
+INSERT INTO company (name) VALUES('tw');
+INSERT INTO employee (name, age, gender,company_id) VALUES
+ ('chengcheng','54','male',1),
+ ('yuexie','82','female',1),
+ ('haifeng','69','female',1),
+ ('ang','18','male',1);
